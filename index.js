d9b5752e8d474e28bf1e738905da83b277c18b5a
Sanjit Rane <sanjit@hotstar.com>
2021-01-28 19:08:33 +0530

    Added bit inArray function

--- index.js
+++ index.js
@@ -135,3 +135,11 @@ function countBits(n){
     let binary = n.toString(2)
     return (binary.match(/[1]/g)||[]).length
 }
+
+/**Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order 
+ * of the strings of a1 which are substrings of strings of a2. */
+function inArray(array1, array2){
+    return array1
+    .filter(a1=>array2.find(a2=>a2.match(a1)))
+    .sort()
+}
