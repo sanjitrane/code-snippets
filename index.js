9f8fe4f3ad5d2f0fef8e76eb616066be3eb9b90d
Sanjit Rane <sanjit@hotstar.com>
2021-01-28 17:04:52 +0530

    Added findNextSquare function

--- index.js
+++ index.js
@@ -61,3 +61,13 @@ function duplicateEncode(word){
     arr.forEach(item=>res[item]++)
     return arr.length === 10 && res[w] === res[e] && res[s] === res[n]
  }
+
+ /**
+  * finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+  */
+
+function findNextSquare(sq){
+    return Math.sqrt(sq)%1===0 ? Math.pow(Math.sqrt(sq)+1, 2) : -1
+}
