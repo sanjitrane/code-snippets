3a55ead7b604f423834e581e07e26acc53b656a6
Sanjit Rane <sanjit@hotstar.com>
2021-01-28 13:05:29 +0530

    Added DescendingOrder function

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function maskify(cc){
     cc.toString()
     return cc.splice(0,-4).replace(/./g,'#')+cc.splice(-4)
 }
+
+/**function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number. */
+function descendingOrder(n){
+    return parseInt(n.toString().split('').sort().reverse().join(''))
+}
