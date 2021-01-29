5f50451ca828cce09d89290b14b1792cdad418b1
Sanjit Rane <sanjit@hotstar.com>
2021-01-29 15:46:20 +0530

    Added EvenIndex finder function

--- index.js
+++ index.js
@@ -186,3 +186,18 @@ function order(words){
     })
     return wordsArr.join(' ')
 }
+
+/**Equal sides of an array */
+
+function findEvenIndex(arr){
+    let left = 0;
+    let right = arr.reduce((a,v)=>{a+v},0)
+    for(let i = 0; i<arr.length; i++){
+        if(i>0) left+=arr[i-1]
+        right -=arr[i]
+        if(left=== right){
+            return i
+        }
+    }
+    return -1
+}
